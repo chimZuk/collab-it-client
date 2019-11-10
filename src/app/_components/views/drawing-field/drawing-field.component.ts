fe33dd6bcad31b0dba207e62ce91eb05f5c79d73
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-10 01:11:09 -0500

    Added pizza types

--- src/app/_components/views/drawing-field/drawing-field.component.ts
+++ src/app/_components/views/drawing-field/drawing-field.component.ts
@@ -12,4 +12,52 @@ export class DrawingFieldComponent implements OnInit {
   ngOnInit() {
   }
 
+  pizzatypes: any = [{
+    name: "plain",
+    ingridients: ["mozzarella", "basil"]
+  },
+  {
+    name: "margherita",
+    ingridients: ["tomatoes", "mozzarella", "basil"]
+  },
+  {
+    name: "pepperoni",
+    ingridients: ["round", "pepperoni", "mozzarella", "basil"]
+  },
+  {
+    name: "sausage",
+    ingridients: ["round", "sausage", "mozzarella", "basil"]
+  },
+  {
+    name: "bacon",
+    ingridients: ["round", "bacon", "mozzarella", "basil"]
+  },
+  {
+    name: "hawaian",
+    ingridients: ["round", "pineapples", "ham", "mozzarella", "basil"]
+  },
+  {
+    name: "white",
+    ingridients: ["round", "white-cheese", "basil"]
+  },
+  {
+    name: "sicilian",
+    ingridients: ["square", "tomatoes", "mozzarella", "basil"]
+  },
+  {
+    name: "mushroom",
+    ingridients: ["round", "mushrooms", "mozzarella", "basil"]
+  },
+  {
+    name: "margherita",
+    ingridients: ["round", "tomatoes", "mozzarella", "basil"]
+  },
+  {
+    name: "chicken-ranch",
+    ingridients: ["round", "chicken", "ranch", "mozzarella", "basil"]
+  },
+  {
+    name: "chicken-parm",
+    ingridients: ["round", "chicken", "parmigiano", "mozzarella", "basil"]
+  }]
 }
