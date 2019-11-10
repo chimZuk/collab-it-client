6aa440650278ffc9e3a18b847aca590b189d183c
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-10 05:28:10 -0500

    Added disabled blur

--- src/app/_components/views/drawing-field/drawing-field.component.ts
+++ src/app/_components/views/drawing-field/drawing-field.component.ts
@@ -11,7 +11,7 @@ export class DrawingFieldComponent implements OnInit {
 
   @Input() authorized: any;
   @Input() userData: any;
-  
+
   pizzaData: any;
   toppings: any = [];
   sauces: any = [];
@@ -85,53 +85,4 @@ export class DrawingFieldComponent implements OnInit {
     this.pizzaData.pizza = this.pizza;
     this.socket.sendPizzaData(this.pizzaData);
   }
-
-  pizzatypes: any = [{
-    name: "plain",
-    ingridients: ["mozzarella", "basil"]
-  },
-  {
-    name: "margherita",
-    ingridients: ["tomatoes", "mozzarella", "basil"]
-  },
-  {
-    name: "pepperoni",
-    ingridients: ["round", "pepperoni", "mozzarella", "basil"]
-  },
-  {
-    name: "sausage",
-    ingridients: ["round", "sausage", "mozzarella", "basil"]
-  },
-  {
-    name: "bacon",
-    ingridients: ["round", "bacon", "mozzarella", "basil"]
-  },
-  {
-    name: "hawaian",
-    ingridients: ["round", "pineapples", "ham", "mozzarella", "basil"]
-  },
-  {
-    name: "white",
-    ingridients: ["round", "white-cheese", "basil"]
-  },
-  {
-    name: "sicilian",
-    ingridients: ["square", "tomatoes", "mozzarella", "basil"]
-  },
-  {
-    name: "mushroom",
-    ingridients: ["round", "mushrooms", "mozzarella", "basil"]
-  },
-  {
-    name: "margherita",
-    ingridients: ["round", "tomatoes", "mozzarella", "basil"]
-  },
-  {
-    name: "chicken-ranch",
-    ingridients: ["round", "chicken", "ranch", "mozzarella", "basil"]
-  },
-  {
-    name: "chicken-parm",
-    ingridients: ["round", "chicken", "parmigiano", "mozzarella", "basil"]
-  }]
 }
