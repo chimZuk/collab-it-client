5fb457fcf63de4f81a85ae09d14a68c80182891f
Dzmitry Kuzmitch <kuzya19989@outlook.com>
2019-11-10 00:46:15 -0500

    Added new favicon

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Collab It!';
+  title = 'Collab It! 🍕🍕🍕 Pizza Edition 🍕🍕🍕';
 }
